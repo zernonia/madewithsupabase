e4bdbf9bd71a9e92d22d92eda12269b49f4e11c7
Boris Kubiak <kubiakboris@gmail.com>
2022-02-07 16:40:57 +0100

    Fix app not loading when analytics are down

--- plugins/umami.client.ts
+++ plugins/umami.client.ts
@@ -1,17 +1,18 @@
 import { defineNuxtPlugin } from "#app"
 
-export default defineNuxtPlugin(async (nuxtApp) => {
+export default defineNuxtPlugin(() => {
   const moduleOptions = {
     websiteId: "545e8bc4-f448-4790-8f28-eaa11d690fb4",
     scriptUrl: "https://umami-zernonia.vercel.app/umami.js",
   }
   const options = { ...moduleOptions }
 
-  await loadScript(options)
+    if (!process.dev) {
+      loadScript(options)
+    }
 })
 
 function loadScript(options: any) {
-  return new Promise((resolve, reject) => {
     const head = document.head || document.getElementsByTagName("head")[0]
     const script = document.createElement("script")
 
@@ -20,13 +21,5 @@ function loadScript(options: any) {
     script.dataset.websiteId = options.websiteId
     script.src = options.scriptUrl
 
-    if (!process.dev) {
-      head.appendChild(script)
-
-      script.onload = resolve
-      script.onerror = reject
-    } else {
-      resolve("")
-    }
-  })
+    head.appendChild(script)
 }
