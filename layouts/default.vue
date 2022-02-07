04d5a990d4961fbea9f412b36c5bc3c01356b2d0
zernonia <zernonia@gmail.com>
2022-02-08 00:48:45 +0800

    fix slot key

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,10 @@
 <template>
-  <div class="bg-dark-900 text-light-200 h-full w-full min-h-screen flex justify-center">
+  <div
+    class="bg-dark-900 text-light-200 h-full w-full min-h-screen flex justify-center"
+  >
     <div class="p-4 md:p-8 w-full h-full max-w-screen-xl flex flex-col">
       <Header></Header>
-      <slot :key="$route.name"></slot>
+      <slot :key="$route.fullPath"></slot>
       <Footer></Footer>
     </div>
   </div>
