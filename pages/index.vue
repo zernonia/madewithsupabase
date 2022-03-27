a43af69a363fc8892f9d9f25986a01b143206d58
zernonia <zernonia@gmail.com>
2022-03-27 11:05:14 +0200

    add some animation to banner

--- pages/index.vue
+++ pages/index.vue
@@ -6,8 +6,20 @@
       <h1 class="text-3xl md:text-4xl text-center">Made with Supabase</h1>
     </div>
 
-    <NuxtLink to="/bring-the-func" class="mt-4 w-full flex justify-center">
-      <img class="w-full md:w-128" :src="HeroImage" alt="" />
+    <NuxtLink
+      to="/bring-the-func"
+      class="group mt-4 p-6 h-84 w-full flex justify-center items-center transition bg-dark-600 rounded-3xl relative"
+    >
+      <img
+        class="w-full md:w-128 group-hover:w-84 transform group-hover:-translate-x-48 transition-all"
+        :src="HeroImage"
+        alt=""
+      />
+      <h2
+        class="text-4xl absolute top-1/2 -translate-y-1/2 translate-x-0 opacity-0 transform group-hover:opacity-100 group-hover:translate-x-48 delay-100 transition-all"
+      >
+        Bring the Func(🕺)
+      </h2>
     </NuxtLink>
 
     <transition name="fade" mode="out-in">
