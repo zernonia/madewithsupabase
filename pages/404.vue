11e39d19700ccadbbe773ec6bdf554789cef65b0
zernonia <zernonia@gmail.com>
2022-04-04 10:27:29 +0800

    add error 404 page

---
+++ pages/404.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: "blank",
+})
+</script>
+
+<template>
+  <div class="mt-20 flex flex-col items-center">
+    <h1 class="text-4xl text-center">Uhhmm... Error 404</h1>
+    <NuxtLink to="/" class="btn mt-4">Home</NuxtLink>
+  </div>
+</template>
