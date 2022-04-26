46b8fcc99122d9ee8a74f9ca53f665f9952f4c6f
zernonia <zernonia@gmail.com>
2022-04-26 10:17:52 +0800

    upgrade to rc1

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,4 @@
-import { defineNuxtConfig } from "nuxt3"
+import { defineNuxtConfig } from "nuxt"
 import Components from "unplugin-vue-components/vite"
 import Icons from "unplugin-icons/vite"
 import IconsResolver from "unplugin-icons/resolver"
