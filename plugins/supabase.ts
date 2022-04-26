8d9a4a9fb4f41daaa2c207023006c72f5f1481d1
zernonia <zernonia@gmail.com>
2022-04-26 10:19:05 +0800

    update provide supabase plugin

--- plugins/supabase.ts
+++ plugins/supabase.ts
@@ -1,10 +1,12 @@
-import { defineNuxtPlugin, useRuntimeConfig } from "#app"
 import { createClient } from "@supabase/supabase-js"
 
-export default defineNuxtPlugin((nuxtApp) => {
+export default defineNuxtPlugin(() => {
   const env = useRuntimeConfig()
   const supabase = createClient(env.SUPABASE_URL, env.SUPABASE_ANON_KEY)
 
-  nuxtApp.provide("supabase", supabase)
-  nuxtApp.vueApp.provide("supabase", supabase)
+  return {
+    provide: {
+      supabase,
+    },
+  }
 })
