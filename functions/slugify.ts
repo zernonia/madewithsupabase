6a50dafea426487c5b3c581ecc773a61bc31f45f
zernonia <zernonia@gmail.com>
2022-05-02 17:30:18 +0800

    add slugify function to folder

---
+++ functions/slugify.ts
@@ -0,0 +1,18 @@
+export const slugify = (str: string) => {
+  str = str.replace(/^\s+|\s+$/g, "")
+  str = str.toLowerCase()
+
+  var from =
+    "ÁÄÂÀÃÅČÇĆĎÉĚËÈÊẼĔȆÍÌÎÏŇÑÓÖÒÔÕØŘŔŠŤÚŮÜÙÛÝŸŽáäâàãåčçćďéěëèêẽĕȇíìîïňñóöòôõøðřŕšťúůüùûýÿžþÞĐđßÆa·/_,:;"
+  var to =
+    "AAAAAACCCDEEEEEEEEIIIINNOOOOOORRSTUUUUUYYZaaaaaacccdeeeeeeeeiiiinnooooooorrstuuuuuyyzbBDdBAa------"
+  for (var i = 0, l = from.length; i < l; i++) {
+    str = str.replace(new RegExp(from.charAt(i), "g"), to.charAt(i))
+  }
+
+  str = str
+    .replace(/[^a-z0-9 -]/g, "")
+    .replace(/\s+/g, "-")
+    .replace(/-+/g, "-")
+  return str
+}
