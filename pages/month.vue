97b5ea7a89c985fe4fd42b91e064703ccde376ed
zernonia <zernonia@gmail.com>
2022-05-28 16:38:35 +0800

    fix: flex-wrap months

--- pages/month.vue
+++ pages/month.vue
@@ -2,9 +2,9 @@
   <div class="mt-12 min-h-screen-md">
     <div class="flex flex-col items-center justify-center text-xl w-full">
       <h2 class="mb-2">Month:</h2>
-      <div class="flex items-center justify-center space-x-2">
+      <div class="flex flex-wrap items-center justify-center">
         <NuxtLink
-          class="px-3 h-12 bg-dark-500 hover:bg-dark-300 border-3 border-dark-200 transition rounded-lg inline-flex items-center justify-center"
+          class="m-1 px-3 py-1 text-sm md:text-base bg-dark-500 hover:bg-dark-300 border-3 border-dark-200 transition rounded-lg inline-flex items-center justify-center"
           :class="{ '!bg-dark-300': `${item.value}` === $route.params.slug }"
           v-for="item in availableMonth"
           :to="`/month/${item.value}`"
