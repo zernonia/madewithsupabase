ecef4df19348f5e4dd62f958c8ce841a62f47708
zernonia <zernonia@gmail.com>
2022-05-28 22:56:45 +0800

    update sizing of button on mobile

--- components/HeaderButton.vue
+++ components/HeaderButton.vue
@@ -1,15 +1,15 @@
 <template>
-  <div>
+  <div class="flex">
     <NuxtLink to="/tag/Web%20Apps">
       <div
-        class="btn-pale !p-0 !w-12 !h-12 inline-flex items-center justify-center mr-2"
+        class="btn-pale !p-0 w-9 md:!w-12 h-9 md:!h-12 inline-flex items-center justify-center mr-2"
       >
         <i-mdi-tag></i-mdi-tag>
       </div>
     </NuxtLink>
     <NuxtLink to="/month/5">
       <div
-        class="btn-pale !p-0 !w-12 !h-12 inline-flex items-center justify-center"
+        class="btn-pale !p-0 w-9 md:!w-12 h-9 md:!h-12 inline-flex items-center justify-center"
       >
         <i-ic-baseline-calendar-month></i-ic-baseline-calendar-month>
       </div>
