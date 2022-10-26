169ef96ccda3719309cd8aaf4520462532fcc6b9
zernonia <zernonia@gmail.com>
2022-10-26 10:48:28 +0800

    inject vercel analyitcs

--- app.vue
+++ app.vue
@@ -13,9 +13,6 @@
 </template>
 
 <script setup lang="ts">
-import { inject } from "@vercel/analytics"
-
-onMounted(() => inject())
 const route = useRoute()
 
 const capitalizeFirstLetter = (string: string) => {
