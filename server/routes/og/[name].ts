876bcfc468afd71cf6d067d9b984da0677746b83
zernonia <zernonia@gmail.com>
2022-11-22 09:29:57 +0800

    fix og image

--- server/routes/og/[name].ts
+++ server/routes/og/[name].ts
@@ -1,7 +1,8 @@
 import { satori } from "v-satori"
 import { readFileSync } from "fs"
-import { join } from "path"
+import { join, resolve } from "path"
 import { useSupabaseServer } from "~~/composables/supabase"
+import { Resvg } from "@resvg/resvg-js"
 import OG from "~~/components/OG.vue"
 
 export default eventHandler(async (event) => {
@@ -21,8 +22,8 @@ export default eventHandler(async (event) => {
 
   const svg = await satori(OG, {
     props: {
-      title: data?.title,
-      author: data?.twitter,
+      title: data?.title ?? "",
+      author: data?.twitter ?? "",
       cover_img: data?.images?.[0],
     },
     width: 1200,
@@ -37,6 +38,38 @@ export default eventHandler(async (event) => {
     ],
   })
 
-  setHeader(event, "Content-Type", "image/svg+xml")
-  return svg
+  const resvg = new Resvg(svg, {
+    fitTo: {
+      mode: "width",
+      value: 1200,
+    },
+    font: {
+      fontFiles: [join(resolve("."), "assets/fonts", "custom-font.ttf")], // Load custom fonts.
+      loadSystemFonts: false,
+    },
+  })
+
+  const resolved = await Promise.all(
+    resvg.imagesToResolve().map(async (url) => {
+      console.info("image url", url)
+      const img = await fetch(url)
+      const buffer = await img.arrayBuffer()
+      return {
+        url,
+        buffer: Buffer.from(buffer),
+      }
+    })
+  )
+  if (resolved.length > 0) {
+    for (const result of resolved) {
+      const { url, buffer } = result
+      resvg.resolveImage(url, buffer)
+    }
+  }
+
+  const renderData = resvg.render()
+  const pngBuffer = renderData.asPng()
+
+  setHeader(event, "Cache-Control", "s-maxage=7200, stale-while-revalidate")
+  return pngBuffer
 })
