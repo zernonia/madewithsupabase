cd0a151d8e877a9292cdcc37db985dcd734d3182
zernonia <zernonia@gmail.com>
2022-11-22 11:53:00 +0800

    add good read

--- pages/index.vue
+++ pages/index.vue
@@ -12,14 +12,7 @@
       <div v-if="hero && hero.length" class="flex flex-col items-center">
         <HeroSlider :data="hero"></HeroSlider>
 
-        <div class="mt-16 sm:mt-24">
-          <h1 class="text-4xl text-center mb-4 sm:mb-8">Testimonial</h1>
-          <div class="card-grid">
-            <div v-for="item in testimonial" :key="item.id?.toString()">
-              <Card :item="item"></Card>
-            </div>
-          </div>
-        </div>
+        <GoodRead></GoodRead>
 
         <Divider class="my-16 !bg-dark-700"></Divider>
       </div>
