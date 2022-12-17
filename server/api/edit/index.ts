61fb18e367ed0ed994ad84fd37737530ab57bcb0
zernonia <zernonia@gmail.com>
2022-12-17 09:27:05 +0800

    fix not directing back to product page

--- server/api/edit/index.ts
+++ server/api/edit/index.ts
@@ -4,6 +4,10 @@ export default defineEventHandler(async (event) => {
   const { form } = await readBody(event)
   const client = useSupabaseServer()
 
-  const { data, error } = await client.from("products").upsert(form).single()
+  const { data, error } = await client
+    .from("products")
+    .upsert(form)
+    .select("slug")
+    .single()
   return { data, error }
 })
