57890e78341635d74fef908cc4bbe9fd3a7b02e2
zernonia <zernonia@gmail.com>
2023-01-10 01:15:31 +0800

    cleanup

--- utils/image-provider.ts
+++ utils/image-provider.ts
@@ -29,7 +29,6 @@ export const getImage = (src: string, { modifiers = {} } = {}) => {
 
   // https://dohemiycqebeipbvsvnr.supabase.co/storage/v1/object/public/products/lookout-1w901k-screenshot-2022-12-19-at-12733-pmpng?width=500
   // https://dohemiycqebeipbvsvnr.supabase.co/storage/v1/render/image/public/products/lookout-1w901k-screenshot-2022-12-19-at-12733-pmpng?width=500
-  // GET https://project_id.supabase.co/storage/v1/render/image/public/bucket/image.jpg?width=500&height=600
 
   const updatedSrc = src.replace("v1/object", "v1/render/image")
 
