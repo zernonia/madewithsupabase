e055de90f6f1b6193b75a12ac56c5cb8cf46b2eb
zernonia <zernonia@gmail.com>
2023-05-24 09:48:41 +0800

    fix: remove data domains hindering umami tracking

--- plugins/analytics.client.ts
+++ plugins/analytics.client.ts
@@ -6,7 +6,6 @@ export default defineNuxtPlugin((nuxtApp) => {
   node.async = true;
   node.src = 'https://umami-zernonia.vercel.app/script.js';
   node.setAttribute('data-website-id', '545e8bc4-f448-4790-8f28-eaa11d690fb4');
-  node.setAttribute('data-domains', 'madewithsupabase.com');
 
   document.body.appendChild(node);
 })
