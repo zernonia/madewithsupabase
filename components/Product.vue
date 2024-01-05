bbd917f0c05e7a7838dffafa358f61282b0354e1
dshukertjr <dshukertjr@gmail.com>
2024-01-05 16:44:07 +0900

    Add button to convert the project into tweets

--- components/Product.vue
+++ components/Product.vue
@@ -1,6 +1,6 @@
 <!-- eslint-disable no-alert -->
 <script setup lang="ts">
-import type { Database, Project } from '@/types'
+import type { Project, ProjectTable } from '@/types'
 
 const props = defineProps<{
   data: Project
@@ -23,37 +23,72 @@ const computedUrl = computed(() => {
   return url.href
 })
 
-function containsTeamInfo(value: Project): value is Database['public']['Tables']['products']['Row'] {
-  return Object.keys(value).includes('team_info')
+function containsTeamInfo(project: Project): project is ProjectTable {
+  return Object.keys(project).includes('team_info')
 }
 
-async function copyProjectInfoToClipboard() {
+function getProject(): ProjectTable {
   const project = data.value
-  if (!project.github_url)
-    return alert('This project does not have a GitHub URL')
 
-  if (!containsTeamInfo(project))
-    return alert('This project does not contain team_info')
+  if (!project.github_url) {
+    alert('This project does not have a GitHub URL')
+    throw new Error('This project does not have a GitHub URL')
+  }
 
-  if (!project.team_info)
-    return alert('team_info is null')
+  if (!containsTeamInfo(project)) {
+    alert('This project does not contain team_info')
+    throw new Error('This project does not contain team_info')
+  }
 
-  if (project.team_info.length === 0)
-    return alert('team_info is empty')
+  if (!project.team_info) {
+    alert('team_info is null')
+    throw new Error('team_info is null')
+  }
 
-  const authorTexts = project.team_info.map(
-    (user: any) => `[@${user?.handler}](https://twitter.com/${user?.handler})`,
-  )
+  if (project.team_info.length === 0) {
+    alert('team_info is empty')
+    throw new Error('team_info is empty')
+  }
+  return project
+}
 
+/**
+ * Converts the project and author info into a defined format and copies it to the clipboard
+ * @param authorModifier Conversion function for the author
+ * @param projectAndAuthorTextModifier Conversion function for the project and author text
+ */
+async function copyAdminInfoToClipboard({ projectAndAuthorTextModifier: projectAndAuthorConversion, authorModifier: authorConversionFunction }:
+{
+  authorModifier: (user: any) => string
+  projectAndAuthorTextModifier: (project: ProjectTable, authorText: string) => string
+}) {
+  const project = getProject()
+
+  const authorTexts = project.team_info!.map(authorConversionFunction)
   let authorText: string
   if (authorTexts.length === 1)
     authorText = authorTexts[0]
   else
     authorText = `${authorTexts.slice(0, authorTexts.length - 1).join(', ')} and ${authorTexts[authorTexts.length - 1]}`
 
-  await navigator.clipboard.writeText(`[${project.title}](${project.github_url}) - by ${authorText}`)
+  const text = projectAndAuthorConversion(project, authorText)
 
+  await navigator.clipboard.writeText(text)
   alert('Copied to clipboard')
+}
+
+async function copyAsMarkdown() {
+  await copyAdminInfoToClipboard({
+    authorModifier: user => `[@${user?.handler}](https://twitter.com/${user?.handler})`,
+    projectAndAuthorTextModifier: (project, authorText) => `[${project.title}](${project.github_url}) - by ${authorText}`,
+  })
+}
+
+async function copyForTweets() {
+  await copyAdminInfoToClipboard({
+    authorModifier: user => `@${user?.handler}`,
+    projectAndAuthorTextModifier: (project, authorText) => `${project.title}: ${project.github_url} by ${authorText}`,
+  })
 }
 
 onMounted(() => {
@@ -82,16 +117,6 @@ onMounted(() => {
       />
 
       <div class="flex items-center gap-2 mt-4 justify-end">
-        <UTooltip v-show="isAdmin" text="Copy Product and author as Markdown">
-          <UButton
-            icon="i-lucide-copy"
-            variant="ghost"
-            color="gray"
-            label="Copy as Markdown"
-            @click="copyProjectInfoToClipboard"
-          />
-        </UTooltip>
-
         <LegoSocialShare>
           <LegoSocialShareTwitter
             :text="tweetText"
@@ -128,6 +153,28 @@ onMounted(() => {
 
         <UButton :to="computedUrl" size="md" target="_blank" label="Visit Website" />
       </div>
+    </div>
+
+    <div v-show="isAdmin" class="flex justify-end pt-4">
+      <UTooltip text="Copy Product and author as Markdown">
+        <UButton
+          icon="i-lucide-copy"
+          variant="ghost"
+          color="gray"
+          label="Copy for Tweets"
+          @click="copyAsMarkdown"
+        />
+      </UTooltip>
+
+      <UTooltip text="Copy Product and author for tweets">
+        <UButton
+          icon="i-lucide-copy"
+          variant="ghost"
+          color="gray"
+          label="Copy as Markdown"
+          @click="copyForTweets"
+        />
+      </UTooltip>
     </div>
 
     <div class="mt-6 md:mt-12 w-full flex flex-col placeholder-rose-50">
